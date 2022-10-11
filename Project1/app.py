3042e8992fa7d341238b23b3e1c6b9bb1a0be865
sothug <ggfffg747@gmail.com>
2022-10-11 12:35:47 +0300

    Исправление

--- Project1/app.py
+++ Project1/app.py
@@ -6,10 +6,11 @@ app_p1 = FastAPI()
 
 @app_p1.get("/")
 def root(message: str=""):
-    return {"data": message}
+    return {"data": "content"}
 
 @app_p1.get("/another_api/{api_request}")
 def get_data(api_request: str):
+    print(api_request)
     url = fr"http://{config.another_host}:{config.another_port}/{api_request}"
     response = requests.get(url)
     return response.json()
--- Project2/app.py
+++ Project2/app.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI
 app_p2 = FastAPI()
 
 @app_p2.get("/")
-def root(message: str):
-    return {"data": message}
+def root():
+    return {"data": "message"}
 
 @app_p2.get("/get_data")
 def get_data():
-    return {"data": "content"}
+    return {"data": "content1"}
+
+@app_p2.get("/get_data/{id}")
+def get_data(id: int):
+    return {"data": "content", "id": id}
